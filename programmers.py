ddb810ef616cf7b099695fb277a2024005ec44b8
SeungJinLee <seungjin9777@gmail.com>
2021-07-01 14:26:53 +0900

    210701_프로그래머스_입국심사

--- programmers.py
+++ programmers.py
@@ -1,73 +1,22 @@
-from collections import Counter
-from itertools import combinations
+def solution(n, times):
+    times = sorted(times)
+    end = times[-1] * (n//2)
+    start = 0
 
-temp = []
-# 전체사용자 - user_id, 불량사용자목록 - banned_id
-# 가능한 경우의수 (전체에서 해당되는 사용자들을 모두 찾고 조합갯수?)
-def solution(user_id, banned_id):
-    answer = 0
-    one_list = []
+    while start <= end:
+        count = 0
+        mid = (start + end) // 2
 
-    # 예외처리 해주니까 테스트케이스 하나 시간초과나던거 해결했다.
-    if len(Counter(banned_id).keys())==1:
-        for j in user_id:
-            if match(j, banned_id[0]):
-                one_list.append(j)
+        for i in times:
+            count += mid // i
 
-        return len(list(combinations(one_list,len(banned_id))))
-
-
-    ban_len = len(banned_id)
-    match_list = [[] for _ in range(ban_len)]
-
-    for idx, i in enumerate(banned_id):
-        for j in user_id:
-            if match(j, i):
-                match_list[idx].append(j)
-    # print(match_list)
-
-    for i in match_list[0]:
-        curr = []
-        dfs(i, 0, match_list[1:], curr)
-    result = set()
-
-    for i in temp:
-        i = set(i)
-        if len(i) != ban_len:
-            continue
-        result.add(tuple(sorted(i)))
-
-    return len(result)
-
-
-# ['frodo', 'crodo'], ['abc123', 'frodoc']]
-def dfs(start, depth, graph, curr):
-    curr.append(start)
-
-    if depth == len(graph):
-        temp.append(curr.copy()) ## 또오오오오오오오오
-        return
-
-    for i in graph[depth]:
-        dfs(i, depth + 1, graph, curr)
-        curr.remove(i)
-
-
-def match(target, banid):
-
-    if len(target) != len(banid):
-        return False
-    for idx, i in enumerate(banid):
-        if i == '*':
-            continue
+        if count < n:
+            start = mid+1
         else:
-            if target[idx] != i:
-                return False
-    return True
+            result = mid
+            end = mid-1
+    print(result)
+    return result
 
 
-u = ["frodo", "fradi", "crodo", "abc123", "frodoc"]
-b = ["*****", "*****"]
-
-k = solution(u,b)
-print(k)
+solution(6, [7,10])
