3d1d21c55740960700d4b4ab9bcd1a3aad1f30d6
SeungJinLee <seungjin9777@gmail.com>
2021-07-05 15:37:55 +0900

    210705_프로그래머스_보석 쇼핑

--- programmers.py
+++ programmers.py
@@ -1,33 +1,45 @@
-answer = 0
+from collections import defaultdict
+def solution(gems):
+    answer = (0,0)
+    global start
+    start, end = 0, 0
+    gems_len = len(gems)
+    kinds = len(set(gems))
+    curr_min = 100000
 
-def solution(triangle):
-    global end
-    end = len(triangle)
-    start_node = triangle[0][0]
-    new_val = [[] for _ in range(end)]
-    # 최대합을 저장할 2차원 리스트
-    new_val[0].append(start_node)
+    interval_dic = defaultdict(int)
 
-    for i in range(end):
-        for j in range(len(triangle[i])):
-            curr = triangle[i][j]
+    while end < gems_len:
+        interval_dic[gems[end]] += 1
+        # print(interval_dic)
+        # 모든 종류의 보석을 포함하는 끝점을 찾으면
+        if len(interval_dic.keys()) == kinds:
+            # 시작, 끝 저장
+            if curr_min > end-start:
+                curr_min = end-start
+                answer = (start, end)
+            # print(answer)
+            # 시작점을 현재의 끝점까지 이동하면서 여전히 조건을 만족하는 부분을 찾는다.
+            for i in range(start, end):
+                interval_dic[gems[i]] -= 1
+                if not interval_dic[gems[i]]:
+                    interval_dic.pop(gems[i])
+                # 여전히 모든종류를 포함한다면 start를 계속 증가시킨다.
+                if len(interval_dic.keys()) == kinds:
+                    if curr_min > end - (i+1):
+                        curr_min = (end - (i+1))
+                        answer = (i+1, end)
+                    start = i+1
+                else:
+                    interval_dic[gems[i]] += 1
+                    break
+            end += 1
+        else:
+            end += 1
 
-			# 맨 윗줄이면 그냥 넘어간다.
-            if i == 0:
-                continue
-            # 대각선 위로 2개가 존재하는 경우 둘중에 더 큰값을 더한다.
-            if j > 0 and j < len(triangle[i])-1:
-                val = max(new_val[i-1][j-1], new_val[i-1][j])
-                curr += val
-            # 오른쪽 위 대각선만 존재하는 경우
-            elif j == 0:
-                curr += new_val[i-1][j]
-            # 왼쪽 위 대각선만 존재하는 경우
-            elif j == len(triangle[i])-1:
-                curr += new_val[i-1][j-1]
+    a, b = answer
+    return [a + 1, b + 1]
 
-			# 구한 현재까지 최대합을 저장한다.
-            new_val[i].append(curr)
 
-	# 최종적으로 맨 마지막줄에서 최댓값이 구할수있는 최대합이다.
-    return max(new_val[-1])
+g = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+print(solution(g))
