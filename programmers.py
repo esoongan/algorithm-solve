5833732a0dad8b1a6e17769eb55e399cfe082791
SeungJinLee <seungjin9777@gmail.com>
2021-07-05 21:06:10 +0900

    210705_프로그래머스_수식 최대화

--- programmers.py
+++ programmers.py
@@ -1,45 +1,50 @@
-from collections import defaultdict
-def solution(gems):
-    answer = (0,0)
-    global start
-    start, end = 0, 0
-    gems_len = len(gems)
-    kinds = len(set(gems))
-    curr_min = 100000
+# 연산자의 우선순위를 재정의하여 만들수있는 절댓값이 가장 큰 숫자 
+# 주어지는 숫자는 모두 양수 
+from itertools import permutations
+from collections import deque
 
-    interval_dic = defaultdict(int)
 
-    while end < gems_len:
-        interval_dic[gems[end]] += 1
-        # print(interval_dic)
-        # 모든 종류의 보석을 포함하는 끝점을 찾으면
-        if len(interval_dic.keys()) == kinds:
-            # 시작, 끝 저장
-            if curr_min > end-start:
-                curr_min = end-start
-                answer = (start, end)
-            # print(answer)
-            # 시작점을 현재의 끝점까지 이동하면서 여전히 조건을 만족하는 부분을 찾는다.
-            for i in range(start, end):
-                interval_dic[gems[i]] -= 1
-                if not interval_dic[gems[i]]:
-                    interval_dic.pop(gems[i])
-                # 여전히 모든종류를 포함한다면 start를 계속 증가시킨다.
-                if len(interval_dic.keys()) == kinds:
-                    if curr_min > end - (i+1):
-                        curr_min = (end - (i+1))
-                        answer = (i+1, end)
-                    start = i+1
-                else:
-                    interval_dic[gems[i]] += 1
-                    break
-            end += 1
+def solution(expression):
+    answer= 0
+    # 0. 숫자와 연산자로 분리
+    number = ''
+    op = set()
+    stack = deque([])
+    for i in expression:
+        if i.isdigit():
+            number += i
         else:
-            end += 1
+            stack.append(number)
+            stack.append(i)
+            op.add(i)
+            number = ''
+    stack.append(number)
 
-    a, b = answer
-    return [a + 1, b + 1]
+    # 1. 주어진 문자열에 수식이 몇개있는지 확인
+    len_op = len(op)
+    op = list(op)
+    # 2. 수식의 가능한 우선순위를 모두 구한다.
+    for case in permutations(op, len_op):
 
+        # 3. 해당 우선순위대로 계산했을때 나온 값들을 이전계산값과 비교하여 더큰값으로 계쏙 저장한다.
+        new_q = deque([])
+        temp_stack = stack.copy()
+        for i in range(len_op):
+            while temp_stack:
+                curr = temp_stack.popleft()
+                # 현재 우선순위에 해당하는 연산자라면 
+                if curr == case[i]:
+                    before = new_q.pop()
+                    after = temp_stack.popleft()
+                    value = str(eval(before + case[i] + after))
+                    new_q.append(value)
+                else:
+                    new_q.append(curr)
+            if len(new_q) == 1:
+                answer = max(answer, abs(int(new_q[-1])))
+            temp_stack = new_q.copy()
+            new_q = deque([])
+    print(answer)
+    return answer
 
-g = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
-print(solution(g))
+solution("100-200*300-500+20")
