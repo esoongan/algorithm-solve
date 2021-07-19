17256d6dc3c3201829616ab4554f40fa538b9a26
SeungJinLee <seungjin9777@gmail.com>
2021-07-19 15:01:45 +0900

    2107019_백준_상어초등학교

--- boj.py
+++ boj.py
@@ -1,82 +1,74 @@
-# 교집합의 크기 /합집합의 크기
-# 집합 a,b가 공집합일땐 1로 정의
-# 다중집합의 경우 교집합은 min, 합집합은 max
-import re
-def solution(str1, str2):
-    # 1. 대문사-> 소문자로
-    str1 = str1.lower()
-    str2 = str2.lower()
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
 
-    # 2. 2글자씩 끊어서 리스트로 만든다.
-    temp1_list = []
-    temp2_list = []
-    for i in range(0, len(str1)-1):
-        temp1_list.append(str1[i:i+2])
-    for i in range(0, len(str2)-1):
-        temp2_list.append(str2[i:i+2])
+n = int(input())
 
-    str1_list = []
-    str2_list = []
-    # 3. 알파벳만 남긴다 by 정규식
-    for i in temp1_list:
-        i = re.sub(r'[^a-z]', '', i)
-        if len(i) !=2:
-            continue
-        str1_list.append(i)
-    for i in temp2_list:
-        i = re.sub(r'[^a-z]', '', i)
-        if len(i) != 2:
-            continue
-        str2_list.append(i)
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-    if len(str2_list) == 0 and len(str2_list) == 0:
-        return 1
+def check(j,k):
+    near_list = []
+    empty = 0
+    for i in range(4):
+        if j+dx[i] >=0 and j+dx[i] < n and k+dy[i] >=0 and k+dy[i] < n:
+            if table[j+dx[i]][k+dy[i]]:
+                near_list.append(table[j+dx[i]][k+dy[i]])
+            else:
+                empty += 1
 
-    #print(str1_list)
-    #print(str2_list)
-
-    # 4. Counter를 이용해서 중복되는 원소들의 개수를 찾는다.
-    from collections import Counter
-    count1 = Counter(str1_list)
-    count2 = Counter(str2_list)
-
-    # 5. 1차적으로 각각을 집합으로 바꾼다음 교집합의 대상이될 원소들을 찾는다.
-
-    set1 = set(str1_list)
-    set2 = set(str2_list)
+    return near_list,empty
+#[[[], 0, 0, 0], [[], 0, 0, 1], [[], 0, 0, 2], [[], 0, 1, 0], [[], 0, 1, 1], [[], 0, 1, 2], [[], 0, 2, 0], [[], 0, 2, 1], [[], 0, 2, 2]]
+def find_seat(l):
+    l = sorted(l, key=lambda x:(x[0], x[1], x[2], x[3]))
+    cnt,emp,x,y = l[0]
+    return x,y
 
 
+student = defaultdict(list)
 
-    oneandtwo = set1 & set2
-    oneortwo = set1 | set2
+for _ in range(n*n):
+    temp = list(map(int, input().split()))
+    student[temp[0]] = temp[1:]
 
-    # {1,2} -> 교집합의 개수는 min(1에서 1의개수, 2에서 1의개수), min(1에서2의개수, 2에서 2의개수)
-    # 합집합의 원소들을 찾는다.
+table = [[False] * n for _ in range(n)]
 
-    resultOfAnd = 0 # 교집합의 개수
-    for i in list(oneandtwo):
-        resultOfAnd += min(count1[i], count2[i])
+for i in student.keys():
+    l = []
+    temp =[]
+    cnt = 0
+    like_friend = 0 # i가 앉을수있는 위치에서 좋아하는 친구들의 수
+    for j in range(n):
+        for k in range(n):
+            if table[j][k]: # 누가 앉아있으면 건너뛰고
+                continue
+            #i학생이 j,k에 앉았을때 인접한 친구들과 인접한 빈칸의 수
+            near_list, emp = check(j,k)
 
-    # {1,2} -> max로 개수구하기
-    # 1에서 교집합의 대상들을  뺀 갯수
-    # 2에서도 교집합의 대상들을 뺀 갯수 ,,,이 3가지 경우를 모두 더한것
-    resultOfOr = 0
-    for i in list(oneandtwo):
-        resultOfOr += max(count1[i], count2[i])
-        count1.pop(i)
-        count2.pop(i)
+            for like in student[i]:
+                if like in near_list:
+                    cnt += 1
+            temp.append(-cnt)
+            temp.append(-emp)
+            temp.append(j)
+            temp.append(k)
+            l.append(temp)
+            temp = []
+            cnt = 0
 
-    resultOfOr += sum(count1.values())
-    resultOfOr += sum(count2.values())
+    #print(l) # l- 친구리스트와 빈칸, 해당 자리
+    x,y = find_seat(l)
+    table[x][y] = i
 
+answer = 0
+score = {0:0, 1:1, 2:10, 3:100, 4:1000}
+for i in range(n):
+    for j in range(n):
+        count = 0
+        friend_list = check(i,j)[0]
+        for f in friend_list:
+            if f in student[table[i][j]]:
+                count += 1
+        answer += score[count]
 
-    #print(resultOfAnd)
-    #print(resultOfOr)
-
-    if resultOfOr == 0 :
-        return 1
-
-    return int((resultOfAnd / resultOfOr) * 65536)
-
-k = solution('aa1+aa2', 'AAAA12')
-print(k)
+print(answer)
